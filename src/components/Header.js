8f8927a23f82e93c362d13937e8f00562d00db6d
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2023-11-09 12:28:42 +0530

    gave class to navlinks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ContextProvider } from "../contexts/FoodContext";
 
 const Header = () => {
+  const { cart } = useContext(ContextProvider);
+
+  const getActive = ({ isActive }) =>
+    isActive
+      ? {
+          color: "red",
+          fontWeight: "700",
+          fontSize: "20px",
+        }
+      : {};
+
   return (
     <div>
       <div className="nav-links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/menu">Menu</NavLink>
-        <NavLink to="/cart">Cart</NavLink>
+        <NavLink to="/" style={getActive} className="header-link">
+          Home
+        </NavLink>
+        <NavLink to="/menu" style={getActive} className="header-link">
+          Menu
+        </NavLink>
+        <div>
+          <NavLink to="/cart" style={getActive} className="header-link">
+            Cart
+          </NavLink>
+          {cart.length ? ` (${cart.length})` : ""}
+        </div>
       </div>
-      <h1>Welcome to neoG Food Ordering App</h1>
+      <h1>Welcome to my Food Ordering App</h1>
     </div>
   );
 };
